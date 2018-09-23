697d99d98d569c21879e5cafadc6b11388fe95df
jas2171552 <43099723+jas2171552@users.noreply.github.com>
2018-09-23 08:24:20 -0700

    Module3

--- ft.js
+++ ft.js
@@ -1,4 +1,4 @@
-		var msg = "  test";
+		var msg = "";
 		function calcYears () {
 		
 					var releaseYear = 1977;
@@ -15,3 +15,47 @@
 					}
 				}
 		document.getElementById("getMessage").addEventListener("click",calcYears,false);
+		
+		function getReleaseDate() {
+			var v_releaseDate = ""
+			var movieSelected = document.getElementById("release").value;
+			
+			switch (movieSelected) {
+				case "Ep IV":
+					v_releaseDate = "05/25/1977";
+					break;
+				case "Ep V":
+					v_releaseDate = "05/21/1980";
+					break;
+				case "Ep VI":
+					v_releaseDate = "05/25/1983";
+					break;
+				case "Ep I":
+					v_releaseDate = "05/19/1999";
+					break;
+				case "Ep II":
+					v_releaseDate = "05/16/2002";
+					break;
+				case "Ep III":
+					v_releaseDate = "05/19/2005";
+					break;
+				case "Ep VI":
+					v_releaseDate = "12/18/2015";
+					break;
+				case "Ep VII":
+					v_releaseDate = "12/15/2017";
+					break;
+				case "Solo":
+					v_releaseDate = "05/10/2018";
+					break;
+				case "Rogue":
+					v_releaseDate = "12/16/2016";
+					break;
+				default:
+					v_releaseDate = "";
+			}
+			document.getElementById("vReleaseDateMsg").innerHTML = movieSelected + ":\r\n" + v_releaseDate; 
+
+		}
+		
+		document.getElementById("releaseDate").addEventListener("click",getReleaseDate,false);
